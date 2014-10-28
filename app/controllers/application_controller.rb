6cdc94a0ccd52dfcd05d84cf3a19697b1eb9b44f
Noah Teshu <nteshu@gmail.com>
2014-10-28 14:26:30 -0400

    first commit of to do list app

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
+
+#all other controllers in project inherit from this
+#inherts from is shown with '<'
