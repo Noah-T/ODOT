a99a762380376a2aa76a029bd0a409124cbdf80e
Noah Teshu <nteshu@gmail.com>
2015-03-22 22:27:20 -0400

    changes to layout

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -4,6 +4,9 @@ class TodoItem < ActiveRecord::Base
   validates :content, presence: true,
   					  length: { minimum: 2 }
 
+  scope :complete, -> { where("completed_at is not null") }
+  scope :incomplete, -> { where(completed_at: nil) }
+
   def completed?
   	#if not blank, return completed (true)
   	!completed_at.blank?
